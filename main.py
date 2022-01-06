c2dc76574900f70d56c2a62b2029d720aa6e7207
Lasthunter657 <ali.redha.ali@gmail.com>
2022-01-07 02:26:15 +0300

    fixing logic

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from itcs254 import Relations
 
-# U = {1, 6, 3, 9}
-# def F(a, b): return a + b <= 10 and any((a + b) % x ** 2 == 0 for x in U)
+ 
+#U = {1, 6, 3, 9}
+#def F(a, b): return a + b <= 10 and any((a + b) % x ** 2 == 0 for x in U)
 
 
-# r = Relations(((a, b) for a in U for b in U if F(a, b) or F(b, a)), U)
+#r = Relations(((a, b) for a in U for b in U if F(a, b) or F(b, a)), U)
 
 
-r = Relations([(3, 4), ], {0, 1, 3, 4})
+r = Relations([(3,4)], {0, 1,3,4})
+
+
+
+
 
 print(r.relations())
